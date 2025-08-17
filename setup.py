91cf6790c4c18a0a920056a4c85cf5f460b79c9b
AdrienR09 <adrienrousseau9@gmail.com>
2025-08-17 03:07:15 -0400

    adding static files to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperspex",
-    version="0.1.0",
+    version="0.2.0",
     author="Adrien Rousseau",
     description="A Python package to help 3D hyperspectral data exploration through a Graphical User Interface.",
     packages=find_packages(),
+    package_data={
+        'hyperspex': ['gui/*.ui', 'gui/style/*.qss', 'gui/colorbar/*.ui'], # Include all .ui files in the 'ui' subdirectory
+    },
     install_requires=[
     "numpy",
     "scipy",
